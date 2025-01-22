da077f7c353d75c5cdc9e8ba099fdec4d928c0e6
Nelson Cruz <nelsoncruz2727@gmail.com>
2025-01-22 12:53:41 -0500

    Initial Commit

---
+++ GetFit/GetFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetFitApp.swift
+//  GetFit
+//
+//  Created by Nelson Cruz on 1/22/25.
+//
+
+import SwiftUI
+
+@main
+struct GetFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
